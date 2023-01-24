2220500446df23ce899994eb9887b2b063a3e0a8
sampathsvskr <37656675+sampathsvskr@users.noreply.github.com>
2023-01-24 18:28:45 +0000

    variables

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import os
 print("hiii")
 
 token = os.environ.get("SECRET_TOKEN_NAME")
-if token:
+if token == "sampath":
     print(token)
+else:
+    print("failed")
 
 token = os.environ.get("VARIABLE_NAME")
 if token:
